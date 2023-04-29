e86630fdc3fd555ef5e15478c3d7de65c61af8e3
Jacob Cohen <jcohen14@mail.pima.edu>
2023-04-28 19:10:16 -0700

    finished first animation

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -9,20 +9,29 @@ public class BirdScript : MonoBehaviour
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public bool birdFlap = false;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.name = "Birb";
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-
+        anim = gameObject.GetComponent<Animator>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Mouse0) == true && birdIsAlive || Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
         {
+            anim.SetTrigger("Active");
             myrigidbody.velocity = Vector2.up * flapStrength; // short hand for vector2(0,1)
+        }
+        else
+        {
+            anim.SetTrigger("Inactive");
         }
         if (transform.position.y > 13 || transform.position.y < -13)
         {
